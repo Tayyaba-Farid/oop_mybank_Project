5e70750707cc3913ac25369011ec45bc58749c88
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-05-06 16:52:29 +0500

    added oop mybank project

---
+++ README.md
@@ -0,0 +1,1 @@
+"# oop_mybank_Project" 
---
+++ index.js
@@ -0,0 +1,137 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+class Customer {
+    firstName;
+    lastName;
+    gender;
+    age;
+    mobileNo;
+    balance;
+    accountNo;
+    constructor(fn, ln, gender, age, mobileNo, depositAmount) {
+        this.firstName = fn,
+            this.lastName = ln,
+            this.gender = gender,
+            this.age = age,
+            this.mobileNo = mobileNo,
+            this.balance = depositAmount,
+            this.accountNo = this.generateAccountNo();
+    }
+    generateAccountNo() {
+        return Math.floor(Math.random() * 900) + 100 + " ";
+    }
+    debit(amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            console.log(`Transaction successful! Your new balance is: ${this.balance}`);
+        }
+        else {
+            console.log("Transaction failed! Insufficient balance.");
+        }
+    }
+    credit(amount) {
+        if (amount > 100) {
+            amount -= 1;
+        }
+        this.balance += amount;
+        console.log(`Transaction successful! Your new balance is: ${this.balance} `);
+    }
+    viewBalance() {
+        console.log(`Your current balance is: ${this.balance}`);
+    }
+    viewAccount() {
+        console.log(`Account Number: ${this.accountNo}`);
+        console.log(`First Name: ${this.firstName}`);
+        console.log(`Last name: ${this.lastName}`);
+        console.log(`Age: ${this.age}`);
+        console.log(`Gender: ${this.gender}`);
+        console.log(`Mobile No: ${this.mobileNo}`);
+        console.log(`Balance: ${this.balance}`);
+    }
+}
+async function myBankAccount() {
+    let userAccount = null;
+    while (true) {
+        const { option } = await inquirer.prompt([{
+                name: "option",
+                message: "Please choose one option: ",
+                type: "list",
+                choices: ["Create Account", "Debit Money", "Credit Money", "View Balance", "View Account", "Exit"]
+            }]);
+        if (option === "Exit") {
+            break;
+        }
+        else if (!userAccount && option !== "Create Account") {
+            console.log("PLease create an Account first!");
+            continue;
+        }
+        if (option === "Create Account") {
+            let { fn, ln, age, gender, mobileNo, depositAmount } = await inquirer.prompt([
+                {
+                    name: "fn",
+                    type: "input",
+                    message: "Please enter your first name: "
+                },
+                {
+                    name: "ln",
+                    type: "input",
+                    message: "Please enter your last name: "
+                },
+                {
+                    name: "age",
+                    type: "number",
+                    message: "Please enter your age: "
+                },
+                {
+                    name: "gender",
+                    type: "list",
+                    message: "Please select your gender: ",
+                    choices: ["Female", "Male"]
+                },
+                {
+                    name: "mobileNo",
+                    type: "number",
+                    message: "Please enter your Mobile Number: "
+                },
+                {
+                    name: "depositAmount",
+                    type: "number",
+                    message: "Please enter your initial deposit: "
+                },
+            ]);
+            userAccount = new Customer(fn, ln, age, gender, mobileNo, depositAmount);
+            console.log("Account created successfully!");
+        }
+        else if (option === "Debit Money") {
+            const { DebitAmount } = await inquirer.prompt({
+                name: "DebitAmount",
+                type: "number",
+                message: "Please enter debit amount: "
+            });
+            if (userAccount) {
+                userAccount.debit(DebitAmount);
+            }
+        }
+        if (option === "Credit Money") {
+            let { CreditAmount } = await inquirer.prompt({
+                name: "CreditAmount",
+                type: "number",
+                message: "Please enter credit amount: "
+            });
+            if (userAccount) {
+                userAccount.credit(CreditAmount);
+            }
+        }
+        if (option === "View Balance") {
+            if (userAccount) {
+                userAccount.viewBalance();
+            }
+        }
+        if (option === "View Account") {
+            if (userAccount) {
+                userAccount.viewAccount();
+            }
+        }
+    }
+}
+myBankAccount();
